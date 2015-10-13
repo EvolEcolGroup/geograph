2a9fc777327793f9f96dcffae0c7c02e7132c8c2
Thibaut Jombart <thibautjombart@gmail.com>
2015-10-13 12:51:06 +0100

    adding unit tests - very basic for now

---
+++ tests/testthat/test_classes.R
@@ -0,0 +1,10 @@
+library("geograph")
+context("Test classes")
+
+test_that("Empty constructors work", {
+  skip_on_cran()
+  x <- new("gGraph")
+  y <- new("gData")
+  expect_is(x, "gGraph")
+  expect_is(y, "gData")
+})
