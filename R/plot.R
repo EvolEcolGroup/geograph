b06439364e13159dac0ffe5c26695519b30a9c6a
Andrea Manica <am315@cam.ac.uk>
2024-01-16 17:15:08 +0000

    A few more fixes

--- R/plot.R
+++ R/plot.R
@@ -45,7 +45,7 @@
 #' the maximum weight).
 #' @param col.rules a data.frame with two named columns, the first one giving
 #' values of a node attribute, and the second one stating colors to be used for
-#' each value. If not provided, this is seeked from the \code{@meta\$color}
+#' each value. If not provided, this is sought from the \code{@meta\$color}
 #' slot of the object.
 #' @param sticky.points a logical indicating if added points should be kept
 #' when replotting (TRUE), or not (FALSE). In any case, \code{reset=TRUE} will
