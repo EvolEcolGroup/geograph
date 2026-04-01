46dc159c5100a7299d5fe0409d7caef22ff654e0
dominikjud <dominik.jud@uzh.ch>
2026-04-01 21:14:32 +0200

    syntax change to be in line with the package style, also updated roxygen header

--- R/createNewGraph.R
+++ R/createNewGraph.R
@@ -1,20 +1,30 @@
-#' Create a new gGraph object from a custom discrete global grid
+#' @title Make a new gGraph object from a custom discrete global grid
 #'
-#' @description
-#' \code{createNewGraph} constructs a new \linkS4class{gGraph} object based on a
+#' @description This function constructs a new [`gGraph`] object based on a
 #' discrete global grid system (DGGS) with a user-defined spatial resolution.
 #' The graph is restricted to a geographic bounding box defined by the user.
 #'
 #' @param geo_box A geographic bounding box. With either a named numeric vector 
-#' with \code{xmin}, \code{xmax}, \code{ymin}, \code{ymax} or an object of class 
-#' \code{bbox} or \code{sf}. Coordinates must be in longitude/latitude (EPSG:4326).
+#' with `xmin`, `xmax`, `ymin`, `ymax` or an object of class 
+#' `bbox` or `sf`. Coordinates must be in longitude/latitude (EPSG:4326).
 #' @param spacing A positive numeric value giving the desired spacing (in km)
 #' of the discrete global grid. The closest available DGGS resolution is used.
-#' @param \dots Further arguments (currently unused).
-#' @return A \linkS4class{gGraph} object representing the DGGS restricted to the
-#' specified geographic region. The \code{@nodes.attr} slot is empty.
-#'
+#' @param ... Additional arguments passed to other methods (currently not used).
+#' @return A [`gGraph`] object representing the DGGS restricted to the
+#' specified geographic region. The `@nodes.attr` slot is empty.
+#' @details The function constructs a discrete global grid using the `dggridR` package,
+#' then identifies neighboring grid cells using the `spdep` package, and finally builds a
+#' [`gGraph`] object with the resulting graph structure and node coordinates.
+#' @examples
+#' #' # Example usage:
+#' ' # Define a geographic bounding box (e.g., for a region in Europe)
+#' geo_box <- c(xmin = -10, xmax = 30, ymin = 35, ymax = 60)
+#' #' # Create a gGraph with a spacing of 1000 km
+#' ggraph <- createNewGraph(geo_box, spacing = 1000)
+#' plot(ggraph, edge = TRUE)
 #' @export
+#' 
+
 createNewGraph <- function(geo_box, spacing, ...) {
   
   if (!is.numeric(spacing) ||
@@ -27,7 +37,7 @@ createNewGraph <- function(geo_box, spacing, ...) {
   # get the boundaries of the region in the format we need
   if (inherits(geo_box, "sf")) {
     bbox <- sf::st_bbox(geo_box)
-  } else if (inherits(geo_box, "bbox")) { #@TODO look if we need to convert crs! 
+  } else if (inherits(geo_box, "bbox")) { #TODO look if we need to convert crs! 
     bbox <- geo_box
   } else if (is.numeric(geo_box) && all(c("xmin","xmax","ymin","ymax") %in% names(geo_box))) {
     bbox <- sf::st_bbox(geo_box, crs = 4326)
@@ -44,21 +54,21 @@ createNewGraph <- function(geo_box, spacing, ...) {
   
   resolution <- dggs$res
   
-  cell_size <- dggridR::dggetres(dggs) %>%
+  cell.size <- dggridR::dggetres(dggs) %>%
     dplyr::filter(res == resolution) %>%
     dplyr::pull(spacing_km)
   
-  grid_sf <- dggridR::dgrectgrid(
+  grid.sf <- dggridR::dgrectgrid(
     dggs,
     minlon = bbox["xmin"],
     maxlon = bbox["xmax"],
     minlat = bbox["ymin"],
     maxlat = bbox["ymax"],
-    cellsize = cell_size / (111 * 3) #@TODO need to find a way to make this dependent on the spacing
+    cellsize = cell.size / (111 * 3) #TODO need to find a way to make this dependent on the spacing
   )
   
   # get the coords argument from the centers of the grid cells
-  centers <- dggridR::dgSEQNUM_to_GEO(dggs, grid_sf$seqnum)
+  centers <- dggridR::dgSEQNUM_to_GEO(dggs, grid.sf$seqnum)
   
   coords <- data.frame(
     lon = centers$lon,
@@ -67,26 +77,26 @@ createNewGraph <- function(geo_box, spacing, ...) {
   
   # get the neighbours using spdep
   xy <- as.matrix(coords)
-  nb <- spdep::dnearneigh( #@TODO switch to sf at some point here 
+  nb <- spdep::dnearneigh( #TODO switch to sf at some point here 
     xy,
     d1 = 0,
-    d2 = cell_size * 1.5, #@TODO make this more robust
+    d2 = cell.size * 1.5, #TODO make this more robust
     longlat = TRUE
   )
   
   neighbours <- unclass(nb)
   
   # build graphNEL object
-  node_ids <- as.character(seq_len(nrow(coords)))
+  node.ids <- as.character(seq_len(nrow(coords)))
   
   edgeL <- lapply(seq_along(neighbours), function(i) {
-    list(edges = node_ids[neighbours[[i]]])
+    list(edges = node.ids[neighbours[[i]]])
   })
-  names(edgeL) <- node_ids
+  names(edgeL) <- node.ids
   
   gNEL <- new(
     "graphNEL",
-    nodes = node_ids,
+    nodes = node.ids,
     edgeL = edgeL,
     edgemode = "undirected"
   )
@@ -97,7 +107,7 @@ createNewGraph <- function(geo_box, spacing, ...) {
     "gGraph",
     graphNEL = gNEL,
     coords = coords,
-    nodes.attr = data.frame(row.names = node_ids),
+    nodes.attr = data.frame(row.names = node.ids),
     meta = list(costs = NULL, colors = NULL),
     neighbours = neighbours
   )
