4095dc02ebec926c52e088acc4b685bfb38e1d75
dominikjud <dominik.jud@uzh.ch>
2026-04-01 21:32:46 +0200

    updated style to be in line with rest of package, also changed roxygen header

--- R/polygonBetween.R
+++ R/polygonBetween.R
@@ -1,46 +1,47 @@
-#' Least-cost paths between two polygons
+#' @title Least-cost paths between two polygons
 #'
-#' @description
-#' \code{polygonBetween} computes least-cost path distances between all nodes 
-#' belonging to two polygons in a \code{gData} or or \code{gGraph} using \code{dijkstraBetween}. 
+#' @description This function computes least-cost path distances between all nodes 
+#' belonging to two polygons in a [`gData`] or [`gGraph`] using `dijkstraBetween.` 
 #' The polygons are specified via a node attribute layer. Optionally, only outline
 #' nodes (nodes with at least one neighbor outside the polygon) are used to
 #' reduce computation time.
 #'
-#' @param g A \code{gData} or \code{gGraph} object with edge costs already defined.
+#' @param g [`gData`] or [`gGraph`] object with edge costs already defined.
 #' @param layer Character. Name of the node attribute layer containing polygon
 #'   membership.
-#' @param poly_i Character. Name of the first polygon.
-#' @param poly_j Character. Name of the second polygon.
-#' @param outline Logical. If \code{TRUE}, only outline nodes of each polygon
+#' @param from Character. Name of the first polygon.
+#' @param to Character. Name of the second polygon.
+#' @param outline Logical. If `TRUE`, only outline nodes of each polygon
 #'   (nodes with at least one neighbor outside the polygon) are used. Defaults
-#'   to \code{TRUE}.
+#'   to `TRUE`.
 #'
 #' @return A numeric vector containing the least-cost distances between nodes
 #'   of the two polygons.
 #'
 #' @details
 #' The function extracts all nodes belonging to the two specified polygons and
-#' computes least-cost paths between them using \code{dijkstraBetween}.
-#' If \code{outline = TRUE}, the computation is restricted to outline nodes of
+#' computes least-cost paths between them using `dijkstraBetween.`
+#' If `outline = TRUE`, the computation is restricted to outline nodes of
 #' each polygon, which can substantially reduce runtime for large polygons.
 #'
 #' @examples
-#' \dontrun{
-#' dist_vec <- polygonBetween(
-#'   g = polyGraph_full,
-#'   layer = "lang",
-#'   poly_i = "Malay",
-#'   poly_j = "Tagalog",
-#'   outline = TRUE
+#' world.countries <- rnaturalearth::ne_countries(
+#'   scale = "medium",
+#'   returnclass = "sf"
 #' )
-#' }
+#' newGraph <- extractFromLayer(rawgraph.10k,
+#'                              layer = world.countries,
+#'                              attr = c("continent", "name")
+#' )
+#' test <- polygonBetween(newGraph, layer = "name", "Spain", "Germany", outline = TRUE)
+#' plot(newGraph, col = NA, reset = TRUE)
+#' plot(test, col = "red")
+#' 
 #'
-#' @importFrom geoGraph dijkstraBetween gPath2dist getNodesAttr getNodes
 #' @export
 
 
-polygonBetween <- function(g, layer, poly_i, poly_j, outline = TRUE) {
+polygonBetween <- function(g, layer, from, to, outline = TRUE) {
   
   #check if g is a gGraph or gData object
   if (!inherits(g, "gGraph") && !inherits(g, "gData")) {
@@ -48,47 +49,47 @@ polygonBetween <- function(g, layer, poly_i, poly_j, outline = TRUE) {
   }
   
   # get node attributes
-  node_attr <- geoGraph::getNodesAttr(g)[[layer]]
-  node_ids  <- geoGraph::getNodes(g)
+  node.attr <- geoGraph::getNodesAttr(g)[[layer]]
+  node.ids  <- geoGraph::getNodes(g)
   
   # extract nodes belonging to each polygon
-  nodes_i <- node_ids[node_attr %in% poly_i]
-  nodes_j <- node_ids[node_attr %in% poly_j]
+  nodes.from <- node.ids[node.attr %in% from]
+  nodes.to <- node.ids[node.attr %in% to]
   
-  if (length(nodes_i) == 0 || length(nodes_j) == 0) {
+  if (length(nodes.from) == 0 || length(nodes.to) == 0) {
     stop("One or both polygons contain no nodes.")
   }
   
   # neighbor list
-  neigh_list <- g@graph@edgeL
+  neighL <- g@graph@edgeL
   
   # function to compute outline nodes
-  polygon_outline_nodes <- function(nodes_polygon) {
+  polygonOutlineNodes <- function(nodes.polygon) {
     
-    outline_nodes <- c()
+    outline.nodes <- c()
     
-    for (node in nodes_polygon) {
-      neighbors <- neigh_list[[node]]$edges
+    for (node in nodes.polygon) {
+      neighbors <- neighL[[node]]$edges
       
-      if (!all(neighbors %in% nodes_polygon)) {
-        outline_nodes <- c(outline_nodes, node)
+      if (!all(neighbors %in% nodes.polygon)) {
+        outline.nodes <- c(outline.nodes, node)
       }
     }
     
-    outline_nodes
+    outline.nodes
   }
   
   # optionally restrict to outline nodes
   if (outline) {
-    nodes_i <- polygon_outline_nodes(nodes_i)
-    nodes_j <- polygon_outline_nodes(nodes_j)
+    nodes.from <- polygonOutlineNodes(nodes.from)
+    nodes.to <- polygonOutlineNodes(nodes.to)
     
-    if (length(nodes_i) == 0 || length(nodes_j) == 0) {
+    if (length(nodes.from) == 0 || length(nodes.to) == 0) {
       return(NA)
     }
   }
   
   # compute least-cost paths
-  paths <- geoGraph::dijkstraBetween(g, from = nodes_i, to = nodes_j)
+  paths <- geoGraph::dijkstraBetween(g, from = nodes.from, to = nodes.to)
   return(paths)
 }
