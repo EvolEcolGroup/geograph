0e63336002eff737a2c406ec6186de4d884ba36d
dominikjud <dominik.jud@uzh.ch>
2026-04-02 10:52:51 +0200

    updated naming of variables to be in line with the rest of the package

--- R/assignRasterPoints.R
+++ R/assignRasterPoints.R
@@ -1,21 +1,19 @@
-#' Assign raster points to graph nodes
+#' @title Assign raster points to graph nodes
 #'
-#' @description
-#' \code{assignRasterPoints} takes a \linkS4class{gGraph} object and a raster
-#' \code{SpatRaster}, and assigns each raster cell to the nearest graph node. 
+#' @description This function takes a [`gGraph`] object and a raster
+#' `SpatRaster`, and assigns each raster cell to the nearest graph node. 
 #' The points are stored in the graph as a node attribute called
-#' \code{raster_points}, which contains all raster values for that node. 
+#' `raster_points`, which contains all raster values for that node. 
 #' Users can then summarize these points to create new attributes.
 #'
-#' @param graph A \linkS4class{gGraph} object.
-#' @param raster A \code{SpatRaster} object (from \code{terra}) 
+#' @param graph A [`gGraph`] object.
+#' @param raster A `SpatRaster` object (from `terra`) 
 #' @param layer_name Character, optional. If provided, stores the raster points in
-#'   \code{graph@nodes.attr[[layer_name]]} instead of \code{raster_points}.
-#' @return A \linkS4class{gGraph} object with a new node attribute containing
+#'   `graph@nodes.attr[[layer_name]]` instead of `raster_points.`
+#' @return A [`gGraph`] object with a new node attribute containing
 #'   the raster points assigned to each node.
 #'
-#' @details
-#' This function is memory-intensive if the raster is high-resolution and/or the
+#' @details This function is memory-intensive if the raster is high-resolution and/or the
 #' graph has many nodes. Users should be mindful of input sizes. Once points are
 #' assigned, the user can compute summaries (mean, median, SD, thresholding)
 #' to create custom attributes like "mountain" or "land". 
@@ -23,55 +21,50 @@
 #' @examples
 #' \dontrun{
 #' graph <- assignRasterPoints(rawgraph.40k, elevation_raster)
-#' # Now each node has a list-column raster_points with all assigned raster points
-#' # You can compute node-level attributes, e.g.:
-#' node_sd <- sapply(graph@nodes.attr$raster_points, function(df) sd(df$elevation))
-#' graph@nodes.attr$sd_elevation <- node_sd
-#' }
-#'
+#' 
 #' @export
 assignRasterPoints <- function(graph, raster, layer_name = "raster_points") {
   
   if (!inherits(graph, "gGraph")) stop("graph must be a gGraph object")
   
-  #@TODO check that raster and graph cover similar areas?
+  #TODO check that raster and graph cover similar areas?
   
   # Convert raster to data.frame
-  raster_df <- as.data.frame(raster, xy = TRUE)
-  colnames(raster_df) <- c("lon", "lat", "value")
+  raster.df <- as.data.frame(raster, xy = TRUE)
+  colnames(raster.df) <- c("lon", "lat", "value")
   
   # Convert raster points to sf
-  raster_sf <- sf::st_as_sf(raster_df, coords = c("lon", "lat"), crs = 4326)
+  raster.sf <- sf::st_as_sf(raster.df, coords = c("lon", "lat"), crs = 4326)
   
   # Convert nodes to sf
-  node_coords <- as.data.frame(graph@coords)
-  node_coords$node_id <- seq_len(nrow(node_coords))
-  nodes_sf <- sf::st_as_sf(node_coords, coords = c("lon", "lat"), crs = 4326)
+  node.coords <- as.data.frame(graph@coords)
+  node.coords$node.id <- seq_len(nrow(node.coords))
+  nodes.sf <- sf::st_as_sf(node.coords, coords = c("lon", "lat"), crs = 4326)
   
   # Disable s2 for nearest neighbor
   sf::sf_use_s2(FALSE) #@TODO is this necessary?
   
   # Find nearest node for each raster point
-  raster_sf$node_id <- st_nearest_feature(
-    raster_sf,
-    nodes_sf
+  raster.sf$node.id <- sf::st_nearest_feature(
+    raster.sf,
+    nodes.sf
   )
   
   # create the attribute object as a list-column
-  atribute <- raster_sf %>% 
+  atribute <- raster.sf %>% 
     sf::st_drop_geometry() %>%
-    dplyr::group_by(node_id) %>%
+    dplyr::group_by(node.id) %>%
     tidyr::nest()
     
-  all_nodes <- tibble::tibble(
-    node_id = seq_len(nrow(nodes_sf))
+  all.nodes <- tibble::tibble(
+    node.id = seq_len(nrow(nodes.sf))
   )
   
-  atribute_full <- all_nodes %>%
-    dplyr::left_join(atribute, by = "node_id")
+  atribute.full <- all.nodes %>%
+    dplyr::left_join(atribute, by = "node.id")
   
   # Store in graph node attribute
-  graph@nodes.attr[[layer_name]] <- atribute_full$data
+  graph@nodes.attr[[layer_name]] <- atribute.full$data
   
   return(graph)
 }
